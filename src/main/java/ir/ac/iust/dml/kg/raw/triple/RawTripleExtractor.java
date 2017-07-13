50cd90423d3556434372c8fb6da1301ea750c746
Majid Asgari <majid.asgari@gmail.com>
2017-07-13 19:40:19 +0430

    standard raw triple + it builder class

--- src/main/java/ir/ac/iust/dml/kg/raw/triple/RawTripleExtractor.java
+++ src/main/java/ir/ac/iust/dml/kg/raw/triple/RawTripleExtractor.java
@@ -3,5 +3,5 @@ package ir.ac.iust.dml.kg.raw.triple;
 import java.util.List;
 
 public interface RawTripleExtractor {
-  public List<RawTriple> generate(String text);
+  List<RawTriple> predict(String source, String version, String text);
 }
