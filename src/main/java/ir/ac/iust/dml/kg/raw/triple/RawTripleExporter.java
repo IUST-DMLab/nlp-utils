57f1ef706253ac7aa869553b1ec8dc2bf362f9ca
mohammad <md.abdous@gmail.com>
2017-07-27 17:28:06 +0430

    nlp utils

--- src/main/java/ir/ac/iust/dml/kg/raw/triple/RawTripleExporter.java
+++ src/main/java/ir/ac/iust/dml/kg/raw/triple/RawTripleExporter.java
@@ -5,29 +5,40 @@ import com.google.gson.GsonBuilder;
 
 import java.io.*;
 import java.nio.file.Path;
+import java.util.List;
 
 @SuppressWarnings(value = {"unused"})
 public class RawTripleExporter implements Closeable {
-  private final Gson gson = new GsonBuilder().setPrettyPrinting().create();
-  private final Writer out;
-  private boolean first = false;
+    private final Gson gson = new GsonBuilder().setPrettyPrinting().create();
+    private final Writer out;
+    private boolean first = false;
 
-  public RawTripleExporter(Path path) throws IOException {
-    this.out = new BufferedWriter(new OutputStreamWriter(
-        new FileOutputStream(path.toFile()), "UTF-8"));
-    this.out.write("[");
-  }
+    public RawTripleExporter(Path path) throws IOException {
+        this.out = new BufferedWriter(new OutputStreamWriter(
+                new FileOutputStream(path.toFile()), "UTF-8"));
+        this.out.write("[");
+    }
 
-  public void write(RawTriple triple) throws IOException {
-    if (!first) first = true;
-    else this.out.write(",");
-    this.out.write("\n");
-    this.out.write(gson.toJson(triple));
-  }
+    public void write(RawTriple triple) throws IOException {
+        if (!first) first = true;
+        else this.out.write(",");
+        this.out.write("\n");
+        this.out.write(gson.toJson(triple));
+    }
 
-  @Override
-  public void close() throws IOException {
-    this.out.write("\n]");
-    this.out.close();
-  }
+    public void writeTripleList(List<RawTriple> rawTripleList) {
+        for (RawTriple rawTriple : rawTripleList) {
+            try {
+                write(rawTriple);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    @Override
+    public void close() throws IOException {
+        this.out.write("\n]");
+        this.out.close();
+    }
 }
--- src/main/java/ir/ac/iust/dml/kg/raw/triple/RawTripleExtractor.java
+++ src/main/java/ir/ac/iust/dml/kg/raw/triple/RawTripleExtractor.java
@@ -3,5 +3,5 @@ package ir.ac.iust.dml.kg.raw.triple;
 import java.util.List;
 
 public interface RawTripleExtractor {
-  List<RawTriple> predict(String source, String version, String text);
+  List<RawTriple> extract(String source, String version, String text);
 }
