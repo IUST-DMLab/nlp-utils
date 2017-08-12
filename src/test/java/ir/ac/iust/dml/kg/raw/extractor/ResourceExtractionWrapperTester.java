bd82f06f435f15ffa60ee997f62e0f97eaed059d
Majid Asgari <majid.asgari@gmail.com>
2017-08-12 14:16:35 +0430

    fix test of filtering resources in resource extractor

--- src/test/java/ir/ac/iust/dml/kg/raw/extractor/ResourceExtractionWrapperTester.java
+++ src/test/java/ir/ac/iust/dml/kg/raw/extractor/ResourceExtractionWrapperTester.java
@@ -14,17 +14,17 @@ public class ResourceExtractionWrapperTester {
     List<MatchedResource> result = wrapper.extract(sample, false).get(0);
     assert result.size() == 12;
     result = wrapper.extract(sample, false, FilterType.CommonPosTags).get(0);
-    assert result.size() == 8;
+    assert result.size() == 7;
     result = wrapper.extract(sample, true, FilterType.CommonPosTags).get(0);
-    assert result.size() == 5;
-    result = wrapper.extract(sample, false, FilterType.CommonPosTags, FilterType.Properties).get(0);
     assert result.size() == 4;
+    result = wrapper.extract(sample, false, FilterType.CommonPosTags, FilterType.Properties).get(0);
+    assert result.size() == 3;
     result = wrapper.extract(sample, false,
         FilterType.CommonPosTags, FilterType.Properties, FilterType.Villages).get(0);
-    assert result.size() == 2;
+    assert result.size() == 1;
     result = wrapper.extract(sample, false,
         FilterType.CommonPosTags, FilterType.Properties, FilterType.Villages, FilterType.Things).get(0);
-    assert result.size() == 2;
+    assert result.size() == 1;
     result = wrapper.extract(sample, false,
         FilterType.CommonPosTags, FilterType.Properties, FilterType.Villages,
         FilterType.Things, FilterType.AnyResources).get(0);
