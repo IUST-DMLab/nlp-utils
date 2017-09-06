e0774adfb13dfff60cdc458f2bc4f7063e627933
Majid Asgari <majid.asgari@gmail.com>
2017-09-06 15:50:29 +0430

    new extract method for repository json files.

--- src/main/java/ir/ac/iust/dml/kg/raw/triple/RawTripleExtractor.java
+++ src/main/java/ir/ac/iust/dml/kg/raw/triple/RawTripleExtractor.java
@@ -1,7 +1,12 @@
 package ir.ac.iust.dml.kg.raw.triple;
 
+import ir.ac.iust.dml.kg.raw.extractor.ResolvedEntityToken;
+
 import java.util.List;
 
 public interface RawTripleExtractor {
+
   List<RawTriple> extract(String source, String version, String text);
+
+  List<RawTriple> extract(String source, String version, List<List<ResolvedEntityToken>> text);
 }
