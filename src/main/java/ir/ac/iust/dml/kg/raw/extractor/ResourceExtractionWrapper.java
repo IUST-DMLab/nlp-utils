df2077c25bf8adee68565394ceaf740f850eac90
Majid Asgari <majid.asgari@gmail.com>
2018-04-28 20:27:09 +0430

    simple sentence branch

--- src/main/java/ir/ac/iust/dml/kg/raw/extractor/ResourceExtractionWrapper.java
+++ src/main/java/ir/ac/iust/dml/kg/raw/extractor/ResourceExtractionWrapper.java
@@ -211,7 +211,7 @@ public class ResourceExtractionWrapper {
         if (resource.getInstanceOf() != null && resource.getInstanceOf().contains("Film")) return true;
         break;
       case MusicalWorks:
-        if (resource.getInstanceOf() != null && resource.getInstanceOf().contains("MusicalWorks")) return true;
+        if (resource.getInstanceOf() != null && resource.getInstanceOf().contains("MusicalWork")) return true;
         break;
       case Properties:
         if ((resource.getType() != null && resource.getType() == ResourceType.Property)
